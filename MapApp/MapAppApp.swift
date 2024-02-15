ca56d17f4f4135c5a577d7145f706060c89c9f95
matrecode <akshaymatrecode@gmail.com>
2024-02-15 11:31:00 +0530

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Hemant Rajkumar Pancheshwar on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
